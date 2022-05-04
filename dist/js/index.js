08516823b8e67a2c237ef824cd3e3f7c94d75dbf
brntava <bruno_tava7@hotmail.com>
2022-05-04 17:10:40 -0300

    adicionado eventos e funçoes dentro do formulario

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,20 +1,44 @@
 let fields = document.querySelectorAll('#form-user-create [name]');
 let user = {};
 
-fields.forEach( (item, index) => {
+const addLine = (dataUser) =>{
+    
+    let tr = document.createElement('tr');
 
-    if(item.name == 'gender'){
+    tr.innerHTML = `
+    <tr>
+        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        <td>${user.name}</td>
+        <td>${user.email}</td>
+        <td>${user.admin}</td>
+        <td>${user.birth}</td>
+        <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>
+    </tr>`;
 
-        if(fields.checked){
-            user.gender
+    document.getElementById('table-users').appendChild(tr)
+}
+
+document.getElementById('form-user-create').addEventListener('submit', e =>{
+
+    e.preventDefault();
+    
+    fields.forEach( (item, index) => {
+
+        if(item.name == 'gender'){
+    
+            if(item.checked){
+                user[item.name] = item.value
+            }
+    
+        } else{
+    
+            // Add o valor do nome direto no user
+            user[item.name] = item.value
         }
+    });
 
-    } else{
-
-        // Add o valor do nome direto no user
-        user[item.name] = fields.value
-    }
+    addLine(user)
 })
-
-console.log(user)
-
